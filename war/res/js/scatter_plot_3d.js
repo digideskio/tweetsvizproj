176efde8f7e27c59a4817507991e3f1c69ccefa8
Mingyu Liu <whoopsandy@gmail.com>
2014-05-30 13:51:18 -0400

    Trim tab view and complete compare view

--- war/res/js/scatter_plot_3d.js
+++ war/res/js/scatter_plot_3d.js
@@ -3,19 +3,20 @@
  */
 var globalv;
 $(document).ready(function() {
-	$('#search').click(function(event) {
-		if($('#compare').is(":checked")) {
-			// compare view
-			var keyword = $('#keyword').val();
-			var keyword2 = $('#keyword2').val();
-			$.get('TweetsVizJSPServlet', {
-				keyword : keyword,
-				keyword2 : keyword2,
-				compareview : "true"
-			}, function(responseText) {
-				console.log(responseText);
-				alert(responseText);
-				/*
+    $('#search').click(function(event) {
+        if ($('#compare').is(":checked")) {
+            // compare view
+            var keyword = $('#keyword').val();
+            var keyword2 = $('#keyword2').val();
+            $.get('TweetsVizJSPServlet', {
+                keyword: keyword,
+                keyword2: keyword2,
+                compareview: "true"
+            }, function(responseText) {
+                console.log(responseText);
+                displayCompareView($.parseJSON(responseText));
+                Pace.stop();
+                /*
 				// TODO
 				try {
 					result = $.parseJSON(responseText);
@@ -29,38 +30,201 @@ $(document).ready(function() {
 				  alert(responseText);
 				}
 				*/
-			});
-		} else {
-			// single view
-			var keyword = $('#keyword').val();
-			$.get('TweetsVizJSPServlet', {
-				keyword : keyword,
-				compareview : "false"
-			}, function(responseText) {
-				var result;
-				var sdata = [];
-				// JSON TEST
-				var result;
-				var sdata = [];
-				try {
-					result = $.parseJSON(responseText);
-					sdata.push(result);
-		            display3dScatterView(sdata);
-		            Pace.stop();
-				} catch (err) {
-				  console.log(responseText);
-				  alert(responseText);
-				}
-			});
-		}
-		
-	});
+            });
+        } else {
+            // single view
+            var keyword = $('#keyword').val();
+            $.get('TweetsVizJSPServlet', {
+                keyword: keyword,
+                compareview: "false"
+            }, function(responseText) {
+                var result;
+                var sdata = [];
+                // JSON TEST
+                var result;
+                var sdata = [];
+                try {
+                    result = $.parseJSON(responseText);
+                    sdata.push(result);
+                    display3dScatterView(sdata);
+                    Pace.stop();
+                } catch (err) {
+                    console.log(responseText);
+                    alert(responseText);
+                }
+            });
+        }
+
+    });
 });
 
+function displayCompareView(sdata) {
+
+    var keyword1 = $('#keyword').val();
+    var keyword2 = $('#keyword2').val();
+    var view1Satisfied = sdata.data1.percentage * 100;
+    var view2Satisfied = sdata.data2.percentage * 100;
+
+
+    var colors = Highcharts.getOptions().colors,
+        categories = ['Satisfied', 'Unsatisfied'],
+        name = 'Satisfaction',
+        data1 = [{
+            y: view1Satisfied,
+
+            color: colors[5]
+        }, {
+            y: 100 - view1Satisfied,
+
+            color: colors[0]
+        }],
+
+        data2 = [{
+            y: view2Satisfied,
+            color: colors[5],
+
+        }, {
+            y: 100 - view2Satisfied,
+
+            color: colors[0]
+        }];
+
+
+    // Build the data arrays
+    var productData1 = [];
+    var productData2 = [];
+
+    for (var i = 0; i < data1.length; i++) {
+
+        // add browser data
+        productData1.push({
+            name: categories[i],
+            y: data1[i].y,
+            color: data1[i].color
+        });
+
+
+
+    }
+
+    for (var i = 0; i < data2.length; i++) {
+
+        // add browser data
+        productData2.push({
+            name: categories[i],
+            y: data2[i].y,
+            color: data2[i].color
+        });
+
+
+
+    }
+
+    // Create the chart
+    $('#containerCompare1').highcharts({
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: keyword1
+        },
+        yAxis: {
+            title: {
+                text: 'Total percent share'
+            }
+        },
+        plotOptions: {
+            pie: {
+                shadow: false,
+                center: ['50%', '50%']
+            }
+        },
+        tooltip: {
+            valueSuffix: '%'
+        },
+        series: [{
+            name: 'Users',
+            data: productData1,
+            size: '60%',
+
+            dataLabels: {
+                formatter: function() {
+                    return this.y > 5 ? this.point.name : null;
+                },
+                color: 'white',
+                distance: -30
+            }
+        }]
+    });
+
+    $('#containerCompare2').highcharts({
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: keyword2
+        },
+        yAxis: {
+            title: {
+                text: 'Total percent share'
+            }
+        },
+        plotOptions: {
+            pie: {
+                shadow: false,
+                center: ['50%', '50%']
+            }
+        },
+        tooltip: {
+            valueSuffix: '%'
+        },
+        series: [{
+            name: 'Users',
+            data: productData2,
+            size: '60%',
+            dataLabels: {
+                formatter: function() {
+                    return this.y > 5 ? this.point.name : null;
+                },
+                color: 'white',
+                distance: -30
+            }
+        }]
+    });
+
+
+    //Display texts
+    var topWordsEle1 = $("#topWords1");
+    var topWordsEle2 = $("#topWords2");
+
+    var words1 = sdata.data1.topwords;
+    var words2 = sdata.data2.topwords;
+
+    for (var i = 0; i < words1.length; i++) {
+        topWordsEle1.append("<p>" + i + 1 + ": " + words1[i] + "</p><br/>");
+    };
+    for (var i = 0; i < words2.length; i++) {
+        topWordsEle2.append("<p>" + i + 1 + ": " + words2[i] + "</p><br/>");
+    };
+
+    var topTweetsEle1 = $("#topTweets1");
+    var topTweetsEle2 = $("#topTweets2");
+
+    var tweets1 = sdata.data1.topmsg;
+    var tweets2 = sdata.data2.topmsg;
+
+    for (var i = 0; i < tweets1.length; i++) {
+        topTweetsEle1.append("<p>" + i + 1 + ": " + tweets1[i] + "</p><br/>");
+    };
+    for (var i = 0; i < tweets2.length; i++) {
+        topTweetsEle2.append("<p>" + i + 1 + ": " + tweets2[i] + "</p><br/>");
+    };
+}
+
 // series data
-function display3dScatterView (sdata) {
+function display3dScatterView(sdata) {
     // Give the points a 3D feel by adding a radial gradient
-    Highcharts.getOptions().colors = $.map(Highcharts.getOptions().colors, function (color) {
+    Highcharts.getOptions().colors = $.map(Highcharts.getOptions().colors, function(color) {
         return {
             radialGradient: {
                 cx: 0.4,
@@ -88,9 +252,18 @@ function display3dScatterView (sdata) {
                 viewDistance: 5,
 
                 frame: {
-                    bottom: { size: 1, color: 'rgba(0,0,0,0.02)' },
-                    back: { size: 1, color: 'rgba(0,0,0,0.04)' },
-                    side: { size: 1, color: 'rgba(0,0,0,0.06)' }
+                    bottom: {
+                        size: 1,
+                        color: 'rgba(0,0,0,0.02)'
+                    },
+                    back: {
+                        size: 1,
+                        color: 'rgba(0,0,0,0.04)'
+                    },
+                    side: {
+                        size: 1,
+                        color: 'rgba(0,0,0,0.06)'
+                    }
                 }
             }
         },
@@ -101,12 +274,11 @@ function display3dScatterView (sdata) {
             text: 'Click and drag the plot area to rotate in space'
         },
         tooltip: {
-			useHTML : true,
-			formatter : function() {
-				console.log(this);
-				return '<div class="datalabels"><b>msg: ' + this.point.name
-						+ '</b><br>x: ' + this.point.x + '<br>y: ' + this.point.y + '<br>z: ' + this.point.z + '</div>';
-			}
+            useHTML: true,
+            formatter: function() {
+                console.log(this);
+                return '<div class="datalabels"><b>msg: ' + this.point.name + '</b><br>x: ' + this.point.x + '<br>y: ' + this.point.y + '<br>z: ' + this.point.z + '</div>';
+            }
         },
         plotOptions: {
             scatter: {
@@ -137,7 +309,7 @@ function display3dScatterView (sdata) {
 
 
     // Add mouse events for rotation
-    $(chart.container).bind('mousedown.hc touchstart.hc', function (e) {
+    $(chart.container).bind('mousedown.hc touchstart.hc', function(e) {
         e = chart.pointer.normalize(e);
 
         var posX = e.pageX,
@@ -149,7 +321,7 @@ function display3dScatterView (sdata) {
             sensitivity = 5; // lower is more sensitive
 
         $(document).bind({
-            'mousemove.hc touchdrag.hc': function (e) {
+            'mousemove.hc touchdrag.hc': function(e) {
                 // Run beta
                 newBeta = beta + (posX - e.pageX) / sensitivity;
                 newBeta = Math.min(100, Math.max(-100, newBeta));
@@ -161,8 +333,8 @@ function display3dScatterView (sdata) {
                 chart.options.chart.options3d.alpha = newAlpha;
 
                 chart.redraw(false);
-            },                            
-            'mouseup touchend': function () { 
+            },
+            'mouseup touchend': function() {
                 $(document).unbind('.hc');
             }
         });
