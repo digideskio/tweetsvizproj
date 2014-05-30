31095057075fd9c0803d362c59319212a36d563b
coronaluo <yuexingluo@gmail.com>
2014-05-30 13:49:07 -0400

    minor bug fixed

--- src/cs886/w14/proj/RuntimeParams.java
+++ src/cs886/w14/proj/RuntimeParams.java
@@ -18,7 +18,7 @@ public class RuntimeParams {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
-	 */ß
+	 */
 	public static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
